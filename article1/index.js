ad86aa8ef7a5da1182346c4ac2668024442d3d3e
your_username <stztrujillo@hotmail.com>
2023-12-07 17:26:22 +0000

    added comment for api key

--- article1/index.js
+++ article1/index.js
@@ -1,6 +1,7 @@
 const { Web3 } = require("web3");
 
-const web3 = new Web3("https://mainnet.infura.io/v3/");
+const web3 = new Web3("https://mainnet.infura.io/v3/YOUR_API_KEY");
+//DONT PUSH YOUR API KEY TO GITHUB
 
 async function main() {
   const lastBlock = await web3.eth.getBlockNumber();
